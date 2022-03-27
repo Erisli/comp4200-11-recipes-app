692b4b4b4bf4e65bed858d098a4c9ba72477aeda
Erisli <Yuezhengbai@gmail.com>
2022-03-27 17:31:16 -0400

    some updates for layouts

--- app/src/main/java/com/example/comp4200_11_recipes_app/UserProfile.java
+++ app/src/main/java/com/example/comp4200_11_recipes_app/UserProfile.java
@@ -2,28 +2,73 @@ package com.example.comp4200_11_recipes_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserProfile extends AppCompatActivity {
     TextView TextUserName;
-    TextView favourite;
-    TextView todoList;
-
+    ListView listView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_profile);
 
+        listView = (ListView) findViewById(R.id.listview);
+        String[] featuresArray = {"-  Favourite","-  Todo List"};
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.features_listview, featuresArray);
+
         TextUserName = (TextView) findViewById(R.id.TextUserName);
-        favourite = (TextView) findViewById(R.id.favourite);
-        todoList = (TextView) findViewById(R.id.todoList);
-
-
         Intent intent = getIntent();
         String userName = intent.getStringExtra("userName");
 
         TextUserName.setText(userName);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                if(position == 0){
+                    Intent intent = new Intent(view.getContext(), UserRecipesList.class);
+                    intent.putExtra("request", "fav");
+                    startActivity(intent);
+                }else if(position == 1){
+                    Intent intent = new Intent(view.getContext(), UserRecipesList.class);
+                    intent.putExtra("request", "todo");
+                    startActivity(intent);
+                }
+
+            }
+
+        });
+        /*favourite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), UserRecipesList.class);
+                intent.putExtra("request", "fav");
+                startActivity(intent);
+
+            }
+        });
+
+        todoList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), UserRecipesList.class);
+                intent.putExtra("request", "todo");
+                startActivity(intent);
+
+            }
+        });*/
 
     }
 
--- app/src/main/java/com/example/comp4200_11_recipes_app/UserRecipesList.java
+++ app/src/main/java/com/example/comp4200_11_recipes_app/UserRecipesList.java
@@ -1,14 +1,40 @@
 package com.example.comp4200_11_recipes_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserRecipesList extends AppCompatActivity {
+    Button back;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_recipeslist);
 
+        back = (Button) findViewById(R.id.button);
+        Intent intent = getIntent();
+        String request = intent.getStringExtra("request");
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        if(request.equals("fav")){
+            Toast.makeText(this, "Favourite recipes displayed", Toast.LENGTH_SHORT).show();
+            //TODO connect to database
+        }else if(request.equals("todo")){
+            Toast.makeText(this, "Todo recipes displayed", Toast.LENGTH_SHORT).show();
+            //TODO connect to database
+        }else{
+            Toast.makeText(this, "Invalid request", Toast.LENGTH_SHORT).show();
+        }
+
+        //TODO recycler view and card
 
     }
 }
